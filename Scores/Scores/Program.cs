06993012363cebf05ad9a33fd0027f93054a2063
jkaushal947 <47825941+jkaushal947@users.noreply.github.com>
2019-03-12 15:11:50 +0000

    Add files via upload

---
+++ Scores/Scores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a score");
+            int score;
+            score = Convert.ToInt32(Console.ReadLine());
+            if (score == 10)
+            {
+                Console.WriteLine("Wow! Top score!!");
+            }
+            else if ((score >= 5) && (score <= 9))
+            {
+                Console.WriteLine("Well done – you passed!");
+            }
+            else if ((score >= 10))
+            {
+                Console.WriteLine("Invalid Score!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you didn’t pass.");
+            }
+            Console.Read();
+        }
+    }
+}
