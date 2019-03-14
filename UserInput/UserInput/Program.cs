860cd10f74683b7c7db7e4f07b7a88d86693b3e1
jkaushal947 <47825941+jkaushal947@users.noreply.github.com>
2019-03-14 09:40:22 +0000

    Add files via upload

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+
+            Console.WriteLine("The sum of number 1 and number 2 ... " + sum);
+
+            Console.Read();
+        }
+    }
+}
